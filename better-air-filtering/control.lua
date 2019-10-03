d56f2a760ff007fefe69a750c1650276d9e6fb15
Joey De Pauw <joeydepauw@gmail.com>
2019-10-03 10:43:43 +0200

    init from air filtering mod

---
+++ better-air-filtering/control.lua
@@ -0,0 +1,11 @@
+--script.on_event({defines.events.on_tick},
+--   function (e)
+--      if e.tick % 60 == 0 then --common trick to reduce how often this runs, we don't want it running every tick, just 1/second
+--         for index,player in pairs(game.players) do  --loop through all players on the server
+--            if player.get_inventory(defines.inventory.character_armor).get_item_count("fire-armor") >= 1 then --if they're wearing our armor
+--               game.surfaces[1].create_entity{name="fire-flame",position=player.position, force="neutral"} --create the fire where they're standing
+--            end
+--         end
+--      end
+--   end
+--)
